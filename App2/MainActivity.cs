5e9d1bfa0854e23e38212fa4a5644a59827c7cae
yas15 <31019957+yas15@users.noreply.github.com>
2017-10-15 16:48:13 +0100

    Add project files.

---
+++ App2/MainActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+
+namespace App2
+{
+    [Activity(Label = "MyApp_V2", MainLauncher = true, Icon = "@drawable/robot_2")]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            // SetContentView (Resource.Layout.Main);
+        }
+    }
+}
+
